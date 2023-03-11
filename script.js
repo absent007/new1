56195f2f2699c78f7cbad06e502227ff5372799b
absent007 <127410273+absent007@users.noreply.github.com>
2023-03-12 00:27:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-const form = document.getElementById('url-shortener-form');
-const input = document.getElementById('url-shortener-input');
-const output = document.getElementById('url-shortener-output');
-const error = document.getElementById('url-shortener-error');
+// Define the function to handle the short link click event
+function handleLinkClick(event) {
+  event.preventDefault(); // prevent the default behavior of the link click
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const url = input.value.trim();
-  if (!isValidUrl(url)) {
-    showError('Please enter a valid URL.');
-    return;
-  }
-  shortenUrl(url);
+  // Open YouTube app in mobile or the Chrome browser in desktop
+  const youtubeUrl = 'youtube://';
+  const chromeUrl = 'googlechrome://';
+
+  // Open the destination URL in Chrome browser after 3 seconds
+  const destinationUrl = this.href;
+  setTimeout(() => {
+    window.location.href = destinationUrl;
+  }, 3000);
+
+  // Try to open the YouTube app or Chrome browser immediately
+  window.location.href = youtubeUrl;
+  setTimeout(() => {
+    window.location.href = chromeUrl;
+  }, 1000);
+}
+
+// Find all the short links on the page
+const shortLinks = document.querySelectorAll('.short-link');
+
+// Add click event listener to each short link
+shortLinks.forEach((link) => {
+  link.addEventListener('click', handleLinkClick);
 });
 
-function isValidUrl(url) {
-  // Basic URL validation - you may want to use a more sophisticated method
-  return url.length > 0 && url.startsWith('http');
-}
-
-function showError(message) {
-  output.style.display = 'none';
-  error.innerText = message;
-  error.style.display = 'block';
-}
-
-function shortenUrl(url) {
-  const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
-  const accessToken = '<YOUR_ACCESS_TOKEN>'; // Replace with your Bitly access token
-  const data = {
-    long_url: url,
-  };
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(data => {
-      const shortUrl = data.link;
-      output.innerText = shortUrl;
-      output.href = shortUrl;
-      output.style.display = 'block';
-      error.style.display = 'none';
-      input.value = '';
-    })
-    .catch(error => {
-      showError('An error occurred while trying to shorten the URL. Please try again later.');
-      console.error(error);
-    });
-}
